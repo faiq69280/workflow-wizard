4b84b306e22a2195c777c079782f5aae27b7eb7b
faiq69280 <faiq69280@gmail.com>
2025-08-23 12:38:19 -0700

    empty project

---
+++ src/main/java/core/server/workflow_management_wizard/WorkflowManagementWizardApplication.java
@@ -0,0 +1,13 @@
+package core.server.workflow_management_wizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowManagementWizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowManagementWizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/core/server/workflow_management_wizard/WorkflowManagementWizardApplicationTests.java
@@ -0,0 +1,13 @@
+package core.server.workflow_management_wizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowManagementWizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
